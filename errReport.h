22a7ad46c71703ac34661f244e7ec7bba9ccb198
JieY02 <1264065719@qq.com>
2023-05-08 20:36:47 +0800

    1.1

--- errReport.h
+++ errReport.h
@@ -1,8 +1,24 @@
-//
-// Created by jieee on 5/8/23.
-//
+#include <stdio.h>
 
-#ifndef XJCO_1921_COURSEWORK_2_SHENGNING_ERRREPORT_H
-#define XJCO_1921_COURSEWORK_2_SHENGNING_ERRREPORT_H
+// ERROR
+// Program given wrong # of arguments
+#define EXIT_WRONG_ARG_COUNT 0
 
-#endif //XJCO_1921_COURSEWORK_2_SHENGNING_ERRREPORT_H
+// Program fails to open file
+#define EXIT_BAD_INPUT_FILE 1
+
+// Program failed on comment line
+#define EXIT_BAD_Comment_Line 2
+
+// Reading in data failed
+#define EXIT_Bad_Data 3
+
+// Writing out data failed
+#define EXIT_BAD_OUTPUT_FILE 4
+
+#define EXIT_Extinct_Error 100
+
+// Successful
+#define EXIT_NO_ERRORS 0
+
+void reportErr(int value, char *filename);
